9f28993db81a5ab9f9868f37434399321f242c65
DuvanMotavita <duvanfel1@yahoo.com.co>
2024-04-24 22:59:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
+//Var declaration
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//Listener to wait for resize and add some responsive logic
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
-
+//Mouse object to use on mouse events
 const mouse = {
-  x: null,
-  y: null,
+  x: 100,
+  y: 100,
 };
-
+//Listerner to do change render point on mouse click
 canvas.addEventListener("click", (event) => {
   mouse.x = event.x;
   mouse.y = event.y;
   drawCircle();
 });
-
+//Function to draw circle on canvas
 function drawCircle() {
   ctx.fillStyle = "blue";
   ctx.beginPath();
   ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
   ctx.fill();
 }
-
+//Function instance
 drawCircle();
